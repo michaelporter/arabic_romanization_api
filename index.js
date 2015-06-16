08cd1aeadf288328a325f1190c48b1d6b89bf547
michaelporter <michaelporter.0988@gmail.com>
2015-06-15 21:16:07 -0400

    removes reference to test_arabic route

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
   var koa = require('koa'),
       router = require('koa-router')(),
       bodyParser = require('koa-body-parser'),
-      testArabicRoute = require('./routes/test_arabic'),
       testRoute = require('./routes/test');
 
   var app = koa();
@@ -12,7 +11,6 @@
   app.use(router.routes());
 
   router.get("/test", testRoute);
-  router.get("/test_arabic", testArabicRoute);
 
   var server = app.listen(3000, function() {
     console.log("server ready");
